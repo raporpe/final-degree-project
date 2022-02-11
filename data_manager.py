cf0560b154c984060b4041fe37356c35063e5be2
Raúl <raporpe@gmail.com>
2022-02-11 17:46:01 +0100

    Refactor

---
+++ data_manager.py
@@ -0,0 +1,78 @@
+import time
+import requests
+
+API_ENDPOINT = "http://tfg-server.raporpe.dev:2000/v1/upload"
+DEVICE_ID = "raspberry-1"
+UPLOAD_PERIOD = 10
+
+# Metaclass manager for DataManager
+
+
+class Singleton(type):
+    _instances = {}  #  Set of instances of the classes
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:  # If not instantiated
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class DataManager(metaclass=Singleton):
+
+    def __init__(self):
+        self.last_upload_time = time.time()
+        self.current_probe_requests = []
+        self.current_beacons = set()
+
+    def register_probe_request(self, station_bssid, power, intent=None):
+
+        self.current_probe_requests.append(
+            {
+                "station_bssid": station_bssid,
+                "intent": intent,
+                "time": int(time.time()),
+                "power": power
+            }
+        )
+
+        self.send_data()
+
+    def register_beacon(self, bssid, ssid):
+        self.current_beacons.add(
+            {
+                "bssid": bssid,
+                "ssid": ssid
+            }
+        )
+
+        self.send_data()
+
+    # Sends the data to the backend
+    def send_data(self):
+
+        # Only send data every N seconds
+        if time.time() - self.last_upload_time > UPLOAD_PERIOD:
+            print("Passed {} seconds. Uploading data to database.".format(
+                UPLOAD_PERIOD))
+
+            json = {
+                "device_id": DEVICE_ID,
+                "probe_requests": self.current_probe_requests,
+                "beacons": self.current_beacons
+            }
+
+            print(json)
+
+            print("Sending data to backend: {probes} probe requests and {beacons} beacons"
+                  .format(probes=json["probe_requests"], beacons=json["beacons"]))
+
+            # Send data to backend in the post payload
+            requests.post(API_ENDPOINT, json=json)
+
+            print("Uploaded data to backend")
+
+            self.last_time = time.time()
+            self.current_probe_requests = []
+            self.current_beacons = set()
+            # Reset the state
--- main.py
+++ main.py
@@ -1,57 +1,24 @@
 from http.client import REQUEST_ENTITY_TOO_LARGE
-from scapy.all import Dot11ProbeReq, sniff
+from scapy.all import *
 import traceback
 import requests
 import time
+from data_manager import DataManager
 
-API_ENDPOINT = "http://tfg-server.raporpe.dev:2000/v1/upload"
-DEVICE_ID = "raspberry-1"
-UPLOAD_PERIOD = 10
-
-counter = 0
 
 def packet_handler(pkt):
     if pkt.haslayer(Dot11ProbeReq):
-        global counter, f
-        counter += 1
+
         print("Packet with MAC {pkt.addr2}, power {pkt.dBm_AntSignal} and SSID {ssid} ".format(show=pkt.show(dump=True), pkt=pkt, ssid=pkt.info.decode()))
-        add_data(station_bssid=pkt.addr2, intent=pkt.info.decode(), power=pkt.dBm_AntSignal)
-        if counter % 100 == 0:
-            print(counter)
         
-
-last_time = 0
-current_data = []
-def add_data(station_bssid, power, intent=None):
-    global current_data
-    current_data.append(
-		{
-			"station_bssid": station_bssid,
-            "intent": intent,
-            "time": int(time.time()),
-            "power": power
-		}
-    )
-
-    global last_time
-    if time.time() - last_time > UPLOAD_PERIOD:
-        print("Passed {} seconds. Uploading data to database.".format(UPLOAD_PERIOD))
-        send_data(current_data)
-        last_time = time.time()
-        current_data = []
+        manager = DataManager()
+        manager.register_probe_request(station_bssid=pkt.addr2, intent=pkt.info.decode(), power=pkt.dBm_AntSignal)
 
 
-def send_data(probe_requests):
-
-    json = 	{ 
-        "device_id": DEVICE_ID,
-        "probe_requests": probe_requests
-    }
-
-    print("Inserting {} records".format(len(json["probe_requests"])))
-    requests.post(API_ENDPOINT, json=json)
-    print("Uploaded data to backend")
-
+    elif pkt.haslayer(Dot11Beacon):
+        manager = DataManager()
+        manager.register_beacon(pkt.addr3, pkt.info.decode())
+        
 
 def start_sniffer():
     try:
