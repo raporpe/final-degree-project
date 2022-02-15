580710e0485e1bb9a6059a631987276bdbff0045
Raúl <raporpe@gmail.com>
2022-02-15 12:02:29 +0100

    Better graph system

--- scripts/graph_true_macs.py
+++ scripts/graph_true_macs.py
@@ -1,39 +1,54 @@
 import psycopg2
-import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
 import numpy as np
 import time
+import datetime
 
 
 conn = psycopg2.connect("host=tfg-server.raporpe.dev dbname=tfg user=postgres password=raulportugues")
 cur = conn.cursor()
 
+
+# Calculate real macs increse graph
 start_time = 1644505200
-end_time = time.time()
-t = start_time
-graph = []
-time = []
-tick = []
-while t < end_time:
+end_time = int(time.time())
+real_macs = []
+fake_macs = []
+unix_time = []
+
+for t in range(start_time, end_time, 3600):
     cur.execute("SELECT COUNT(distinct station_mac) FROM probe_request_frames WHERE station_mac_vendor is not null and time < '{}'".format(t))
-    number = cur.fetchall()[0][0]
-    print(number)
-    hour = (((t-start_time)/3600)+15)%24
-    day = int((t-start_time)/3600/24)
-    if hour == 0 or hour == 8:
-        tick.append(len(graph))
+    real_mac_number = cur.fetchall()[0][0]
+    print(real_mac_number)
+    real_macs.append(real_mac_number)
+    unix_time.append(t)
 
-    time.append(str(day) + "d " + str(hour) + "h")
-    graph.append(number)
-    t += 3600
+for t in range(start_time, end_time, 3600):
+    cur.execute("SELECT COUNT(distinct station_mac) FROM probe_request_frames WHERE station_mac_vendor is null and time < '{}'".format(t))
+    fake_mac_number = cur.fetchall()[0][0]
+    print(fake_mac_number)
+    fake_macs.append(fake_mac_number)
 
-#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%h'))
-#plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=5))
+real_macs = pd.DataFrame({
+    "time": unix_time,
+    "data": real_macs
+})
 
-print(time)
-print(graph)
+fake_macs = pd.DataFrame({
+    "time": unix_time,
+    "data": fake_macs
+})
 
-plt.plot(time, graph)
-ticks = np.arange(0, len(time)+1, 15)
-plt.vlines(tick, 0, 4000, color='red')
-plt.xticks(tick, rotation=45, ha='right')
-plt.savefig("./img.png") 
+real_macs.time = real_macs.time.apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%d %a - %Hh'))
+fake_macs.time = fake_macs.time.apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%d %a - %Hh'))
+
+
+tick = []
+
+fig = px.line(real_macs, x="time", y="data")
+fig.write_image("real_macs.png")
+
+
+fig = px.line(fake_macs, x="time", y="data")
+fig.write_image("fake_macs.png")
