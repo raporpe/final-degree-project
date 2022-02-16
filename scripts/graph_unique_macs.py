cce9dfb6acefd669e4a048b01a397ad72205d6fc
raporpe <raporpe@gmail.com>
2022-02-16 21:16:33 +0100

    Improve graph

--- scripts/graph_unique_macs.py
+++ scripts/graph_unique_macs.py
@@ -1,7 +1,6 @@
 import psycopg2
 import plotly.express as px
 import pandas as pd
-import numpy as np
 import time
 import datetime
 
@@ -21,43 +20,69 @@ cur = conn.cursor()
 start_time = 1644505200
 end_time = int(time.time())
 real_macs_cumulative = []
+quite_real_macs_cumulative = []
 fake_macs_cumulative = []
-unix_time = []
+unix_time = [i for i in range(start_time, end_time, 3600)]
+
 
 for t in range(start_time, end_time, 3600):
-    cur.execute("SELECT COUNT(distinct station_mac) FROM probe_request_frames WHERE station_mac_vendor is not null and time < '{}'".format(t))
-    real_mac_number = cur.fetchall()[0][0]
-    print(real_mac_number)
-    real_macs_cumulative.append(real_mac_number)
-    unix_time.append(t)
+    cur.execute("SELECT count(distinct station_mac) FROM probe_request_frames WHERE station_mac_vendor is not null and time < '{}'".format(t))
+    number = cur.fetchall()[0][0]
+    print(number)
+    real_macs_cumulative.append(number)
+
+
+for t in range(start_time, end_time, 3600):
+    cur.execute("""select count(*) from 
+                (SELECT distinct station_mac FROM probe_request_frames WHERE station_mac_vendor is not null and time < '{}'
+                INTERSECT
+                SELECT station_mac FROM probe_request_frames GROUP BY station_mac HAVING COUNT(*) > 1
+                ) AS E""".format(t))
+    number = cur.fetchall()[0][0]
+    print(number)
+    quite_real_macs_cumulative.append(number)
+
+
 
 for t in range(start_time, end_time, 3600):
     cur.execute("SELECT COUNT(distinct station_mac) FROM probe_request_frames WHERE station_mac_vendor is null and time < '{}'".format(t))
-    fake_mac_number = cur.fetchall()[0][0]
-    print(fake_mac_number)
-    fake_macs_cumulative.append(fake_mac_number)
+    number = cur.fetchall()[0][0]
+    print(number)
+    fake_macs_cumulative.append(number)
 
 real_macs = deacumulate(real_macs_cumulative)
 fake_macs = deacumulate(fake_macs_cumulative)
+quite_real_macs = deacumulate(quite_real_macs_cumulative)
 
+print(len(unix_time))
+print(len(fake_macs_cumulative))
+print(len(fake_macs))
+print(len(real_macs_cumulative))
+print(len(real_macs))
+print(len(quite_real_macs_cumulative))
+print(len(quite_real_macs))
 
 df = pd.DataFrame({
     "time": unix_time,
     "fake_macs_cumulative": fake_macs_cumulative,
     "fake_macs": fake_macs,
     "real_macs_cumulative": real_macs_cumulative,
-    "real_macs": real_macs
+    "real_macs": real_macs,
+    "quite_real_macs_cumulative": quite_real_macs_cumulative,
+    "quite_real_macs": quite_real_macs
 })
 
 df.time = df.time.apply(lambda d: datetime.datetime.fromtimestamp(
     int(d)).strftime('%d %a - %Hh'))
 
 fig = px.line(df, x="time", 
-                y=["fake_macs", "real_macs", "fake_macs_cumulative", "real_macs_cumulative"],
+                y=["fake_macs", "real_macs", "fake_macs_cumulative",
+                    "real_macs_cumulative", "quite_real_macs", "quite_real_macs_cumulative"],
                 title="Detected unique macs")
 
 dates = df["time"].to_list()
 dates = set([i[:6] for i in dates])
+print(dates)
 
 for date in dates:
     fig.add_vrect(x0=date+" - 00h", x1=date+" - 08h",
