b4c8348cb064c30b3b518ca09437969aca5cf7a4
Raúl <raporpe@gmail.com>
2022-03-02 11:38:30 +0100

    Improve channel switcher

--- channel_switcher.py
+++ channel_switcher.py
@@ -1,7 +1,9 @@
 import os
 import time
-chans = [1,6,11, 2,7,12, 3,9,13, 4,10,5,8]
-wait = 0.2
+two = [1,6,11, 2,7,12, 3,9,13, 4,10,5,8]
+five = [36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140]
+chans = two
+wait = 0.1
 i = 0
 while True:
     os.system('iw dev wlan1 set channel {}'.format(chans[i]))
