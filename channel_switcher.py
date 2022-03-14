9cf17ec0ad5799e231c48a8253ece7e876acb809
Raúl <raporpe@gmail.com>
2022-03-14 17:47:26 +0100

    Set interface in cli command

--- channel_switcher.py
+++ channel_switcher.py
@@ -1,11 +1,13 @@
 import os
 import time
+import sys
 two = [1,6,11, 2,7,12, 3,9,13, 4,10,5,8]
 five = [36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140]
 chans = two
 wait = 0.1
 i = 0
+wlan = sys.argv[1]
 while True:
-    os.system('iw dev wlan1 set channel {}'.format(chans[i]))
+    os.system('iw dev {} set channel {}'.format(wlan, chans[i]))
     i = (i + 1) % len(chans)
     time.sleep(wait)
