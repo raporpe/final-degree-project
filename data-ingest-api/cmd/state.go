a6f926f579963dc8dd4bd0aa7944715bc03ea348
Raúl P <raporpe@gmail.com>
2022-03-23 21:33:56 +0000

    Implement activity detector

--- data-ingest-api/cmd/state.go
+++ data-ingest-api/cmd/state.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 )
 
@@ -65,5 +66,93 @@ func (s *Record) Shift(shift int) {
 }
 
 func (s Record) IsActive() bool {
-	return true
+	// Mask search for XOOX XOXO OXOX
+	for i := 0; i < len(s.Record)-3; i++ {
+		toCompare := []bool{s.Record[i], s.Record[i+1], s.Record[i+2], s.Record[i+3]}
+
+		a := []bool{true, false, false, true}
+		b := []bool{true, false, true, false}
+		c := []bool{false, true, false, true}
+
+		aC := reflect.DeepEqual(toCompare, a)
+		bC := reflect.DeepEqual(toCompare, b)
+		cC := reflect.DeepEqual(toCompare, c)
+
+		if aC {
+			s.Record[i] = true
+			s.Record[i+1] = true
+			s.Record[i+2] = true
+			s.Record[i+3] = true
+		}
+
+		if bC {
+			s.Record[i] = true
+			s.Record[i+1] = true
+			s.Record[i+2] = true
+			s.Record[i+3] = false
+		}
+
+		if cC {
+			s.Record[i] = false
+			s.Record[i+1] = true
+			s.Record[i+2] = true
+			s.Record[i+3] = true
+		}
+
+	}
+
+	// Determine if active
+	rowActive := 0
+	for i := len(s.Record) - 1; i > 0; i-- {
+		if s.Record[i] {
+			rowActive++
+		} else {
+			break
+		}
+	}
+
+	//Count total active
+	active := 0
+	for _, s := range s.Record {
+		if s {
+			active++
+		}
+	}
+
+	rowActiveRatio := float64(rowActive) / float64(len(s.Record))
+	activeRatio := float64(active) / float64(len(s.Record))
+
+	if activeRatio > 0.65 {
+		return true
+	}
+
+	if rowActiveRatio > 0.3 {
+		return true
+	}
+
+	return false
+}
+
+func (s Record) applyMask(mask []bool) []bool {
+	// Mask must be at least 3
+	if len(mask) < 3 {
+		return s.Record
+	}
+
+	for i := 0; i < len(s.Record)-len(mask)+1; i++ {
+		match := true
+		for idx, _ := range mask {
+			if s.Record[idx] != mask[idx] {
+				match = false
+			}
+		}
+
+		// Set to true if there was a mask match
+		if match {
+			for idx, _ := range mask {
+				s.Record[idx] = true
+			}
+		}
+
+	}
 }
