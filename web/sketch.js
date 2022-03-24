d22808594c0340c3bb624b4b3935c31dcacca641
Raúl <raporpe@gmail.com>
2022-03-24 01:23:21 +0100

    Add particles

--- web/sketch.js
+++ web/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
 
+    let url = "https://api.raporpe.dev/v1/state"
+    httpGet(url, 'json', false, function(response) {
+        // when the HTTP request completes, populate the variable that holds the
+        // earthquake data used in the visualization.
+        data = response;
+        console.log(data);
+    });
+
 }
 
 function draw() {
-    
+
 }
