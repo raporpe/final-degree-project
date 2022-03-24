88ba61259488f7386f2b14ace8d3a18f842ebf97
Raúl <raporpe@gmail.com>
2022-03-24 13:53:59 +0100

    Get data from tfg-api

--- web/sketch.js
+++ web/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 
-    let url = "https://api.raporpe.dev/v1/state"
+    let url = "https://tfg-api.raporpe.dev/v1/state"
     httpGet(url, 'json', false, function(response) {
         // when the HTTP request completes, populate the variable that holds the
         // earthquake data used in the visualization.
