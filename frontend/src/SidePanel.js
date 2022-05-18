1c3e773dfab9caa93887ed053ab2967332534609
Raúl P <raporpe@gmail.com>
2022-05-18 23:00:10 +0000

    web: implement sidepanel

--- frontend/src/SidePanel.js
+++ frontend/src/SidePanel.js
@@ -5,14 +5,18 @@ import "./SidePanel.css";
 class SidePanel extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            selectedRoom: this.props
-        }
     }
 
     render() {
         return (
-            <div></div>
+            <div className="sidebar">
+                 <img className="sidebar-image" src="/biblioteca.webp"></img>
+                 <div className="sidebar-content">
+                    <div className="sidebar-title">Sala de estudio</div>
+                    <div className="sidebar-ocupacion">70% de ocupacion</div>
+                    <div className="sidebar-graph"></div>
+                 </div>
+            </div>
         )
     }
 }
