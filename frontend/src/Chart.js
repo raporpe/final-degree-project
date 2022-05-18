00fde27b9a74efbaf310928219e9eef1a0705326
Raúl P <raporpe@gmail.com>
2022-05-18 23:11:26 +0000

    Create chart component

---
+++ frontend/src/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chart.css'
+
+class Chart extends React.Component {
+    render() {
+        return (
+            <div>chart</div>
+        )
+    }
+}
+
+export default Chart;
--- frontend/src/SidePanel.js
+++ frontend/src/SidePanel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SidePanel.css";
+import Chart from './Chart';
 
 
 class SidePanel extends React.Component {
@@ -10,12 +11,14 @@ class SidePanel extends React.Component {
     render() {
         return (
             <div className="sidebar">
-                 <img className="sidebar-image" alt="biblioteca uc3m" src="/biblioteca.webp"></img>
-                 <div className="sidebar-content">
+                <img className="sidebar-image" alt="biblioteca uc3m" src="/biblioteca.webp"></img>
+                <div className="sidebar-content">
                     <div className="sidebar-title">Sala de estudio</div>
                     <div className="sidebar-ocupacion">70% de ocupacion</div>
-                    <div className="sidebar-graph"></div>
-                 </div>
+                    <div className="sidebar-chart">
+                        <Chart room={this.props.currentRoom}></Chart>
+                    </div>
+                </div>
             </div>
         )
     }
