4ffa3013fe0f8a91e44fd1925be727ed95a532b8
Raúl P <raporpe@gmail.com>
2022-05-18 23:02:06 +0000

    web: fix warnings

--- frontend/src/SidePanel.js
+++ frontend/src/SidePanel.js
@@ -3,14 +3,14 @@ import "./SidePanel.css";
 
 
 class SidePanel extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+    //constructor(props) {
+    //    super(props)
+    //}
 
     render() {
         return (
             <div className="sidebar">
-                 <img className="sidebar-image" src="/biblioteca.webp"></img>
+                 <img className="sidebar-image" alt="biblioteca uc3m" src="/biblioteca.webp"></img>
                  <div className="sidebar-content">
                     <div className="sidebar-title">Sala de estudio</div>
                     <div className="sidebar-ocupacion">70% de ocupacion</div>
