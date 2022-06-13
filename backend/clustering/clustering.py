d26d3c60b3fd3b51464ebb9726e709ab93e26343
raporpe <raporpe@gmail.com>
2022-06-13 15:25:32 +0200

    clustering: improve distance calculation

--- backend/clustering/clustering.py
+++ backend/clustering/clustering.py
@@ -43,16 +43,17 @@ def receive_digested_macs(digested_macs: list[DigestedMAC]):
     distance_matrix = calculate_distance_matrix(digested_macs)
 
     # Perform t-SNE
-    tsne = TSNE(n_components=2, perplexity=30, n_iter=300)
+    tsne = TSNE(n_components=2, perplexity=5, n_iter=300)
     tsne_results = tsne.fit_transform(distance_matrix)
-
-    # Print the t-SNE results
-    print("t-SNE results:")
-    print(tsne_results)
 
     # Plot the tne results with plotly
     import plotly.express as px
-    fig = px.scatter(tsne_results, x=0, y=1)
+    # Add the MAC addresses to the t-SNE results
+    tsne_results2 = tsne_results.tolist()
+    for i in range(len(digested_macs)):
+        tsne_results2[i].append(digested_macs[i].mac)
+    # Plot the t-SNE results
+    fig = px.scatter(tsne_results2, x=0, y=1, color=2)
     fig.show()
 
     # Perform OPTICS clustering
@@ -64,7 +65,6 @@ def receive_digested_macs(digested_macs: list[DigestedMAC]):
 
     # Show the results of optics clustering
     print("--------- results ----------")
-    print(labels)
 
     # Return the labels of the clusters
     return labels.tolist()
@@ -75,12 +75,16 @@ def calculate_distance_matrix(digested_macs: list[DigestedMAC]):
     # Create a distance matrix
     distance_matrix = [[0 for i in range(len(digested_macs))] for j in range(len(digested_macs))]
 
-    print(distance_matrix)
-
     # Calculate the distances between the digested MACs
     for i in range(len(digested_macs)):
         for j in range(len(digested_macs)):
             distance_matrix[i][j] = calculate_distance(digested_macs[i], digested_macs[j])
+
+    # Show distance matrix in plotly
+    import plotly.express as px
+    # Plot the distance matrix
+    fig = px.imshow(distance_matrix)
+    fig.show()
     
     return distance_matrix
 
@@ -91,44 +95,62 @@ def calculate_distance(digested_mac_1: DigestedMAC, digested_mac_2: DigestedMAC)
 
     # Calculate the distance between the average signal strenght
     # The average signal strenght are converted to logarithmic scale
-    dbm = abs(10*log(digested_mac_1.average_signal_strenght/0.000000001, base=10) - 10*log(digested_mac_2.average_signal_strenght/0.001, base=10))
-    print(dbm)
-    distance += dbm
-
+    dbm = 10*log(digested_mac_1.average_signal_strenght/100000, 10) - 10*log(digested_mac_2.average_signal_strenght/100000, 10)
+    distance += normalize(dbm, 0, -100)*15
+    # MAX 15
 
 
     # Calculate the distance between the manufacturer
-    if digested_mac_1.manufacturer is not None and digested_mac_2.manufacturer is not None:
-        distance += 0 if digested_mac_1.manufacturer == digested_mac_2.manufacturer else 1
+    # if digested_mac_1.manufacturer is not None and digested_mac_2.manufacturer is not None:
+    #     distance += 0 if digested_mac_1.manufacturer == digested_mac_2.manufacturer else 1
     
     # Calculate the distance between the OUI ID
-    distance += 0 if digested_mac_1.oui_id == digested_mac_2.oui_id else 1
-
-    # Calculate the distance between the type count
+    # MAX 2
+    # distance += 2 if digested_mac_1.oui_id != digested_mac_2.oui_id else 0
 
     # Calculate the distance between the presence record
-    for i in range(len(digested_mac_1.presence_record)):
-        distance += 1 if digested_mac_1.presence_record[i] != digested_mac_2.presence_record[i] else 0
-
-    # Calculate the distance between the SSID probes.
+    # MAX 15
+    # for i in range(len(digested_mac_1.presence_record)):
+    #     if digested_mac_1.presence_record[i] or digested_mac_2.presence_record[i]:
+    #         distance += 1 if digested_mac_1.presence_record[i] != digested_mac_2.presence_record[i] else 0
 
     # Calculate the distance between the HT capabilities
+    # MAX 15
     if digested_mac_1.ht_capabilities is not None and digested_mac_2.ht_capabilities is not None:
-        distance += 0 if digested_mac_1.ht_capabilities == digested_mac_2.ht_capabilities else 1
+        distance += 15 if digested_mac_1.ht_capabilities != digested_mac_2.ht_capabilities else 0
+    else:
+        distance += 5
 
     # Calculate the distance between the HT extended capabilities
+    # MAX 15
     if digested_mac_1.ht_extended_capabilities is not None and digested_mac_2.ht_extended_capabilities is not None:
-        distance += 0 if digested_mac_1.ht_extended_capabilities == digested_mac_2.ht_extended_capabilities else 1
+        distance += 15 if digested_mac_1.ht_extended_capabilities != digested_mac_2.ht_extended_capabilities else 0
+    else:
+        distance += 5
 
     # Calculate the distance between the supported rates
-    if digested_mac_1.tags != None and digested_mac_2 != None:
-        distance += 1 if digested_mac_1.supported_rates != digested_mac_2.supported_rates else 0
+    # MAX 15
+    if digested_mac_1.supported_rates != None and digested_mac_2.supported_rates != None:
+        distance += 15 if set(digested_mac_1.supported_rates) != set(digested_mac_2.supported_rates) else 0
+    else:
+        distance += 5
     
     # Calculate the distance between the tags
-    if digested_mac_1.tags != None and digested_mac_2 != None:
-        distance += 1 if digested_mac_1.tags != digested_mac_2.tags else 0
+    # MAX 15
+    if digested_mac_1.tags != None and digested_mac_2.tags != None:
+        distance += 15 if set(digested_mac_1.tags) != set(digested_mac_2.tags) else 0
+    else:
+        distance += 5
     
-    return distance
+    ret = normalize(distance, 10, 75)
+    print(digested_mac_1.mac, digested_mac_2.mac, ret)
+
+    # if the macs are equal, print the distance
+    if digested_mac_1.mac == digested_mac_2.mac:
+        print("Equal:", digested_mac_1.mac, digested_mac_2.mac, distance)
+
+
+    return ret
 
 
 @app.exception_handler(RequestValidationError)
@@ -139,8 +161,13 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     content = {'status_code': 10422, 'message': exc_str, 'data': None}
     return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+
+# Function to normalize integer values to the range [0, 1]
+def normalize(value: int, min_value: int, max_value: int):
+    return (value - min_value) / (max_value - min_value)
+
+
 # Run the fastAPI server
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("clustering:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
-    
