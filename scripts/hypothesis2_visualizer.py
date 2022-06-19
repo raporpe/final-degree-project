4f3cf0df31e127f2934ada63eb497254d88bc33c
raporpe <raporpe@gmail.com>
2022-06-19 13:22:36 +0200

    scripts: hypothesis2 now generates file in pdf

--- scripts/hypothesis2_visualizer.py
+++ scripts/hypothesis2_visualizer.py
@@ -1,3 +1,4 @@
+from turtle import width
 import pandas as pd
 import plotly.express as px
 import datetime as dt
@@ -23,6 +24,10 @@ resp = requests.get("https://tfg-api.raporpe.dev/v1/historic", params=par)
 
 rooms = resp.json()["rooms"]
 
+# Delete every room except makerspace
+rooms = {k: v for k, v in rooms.items() if k == "makerspace"}
+
+
 print(rooms)
 
 # Convert from json response to dataframe format
@@ -34,6 +39,9 @@ for room_name in rooms:
 for s in series:
     obtained = s[0]
 
+    # Only keep the dates from may 27 01:00 to may 29
+    obtained = obtained[obtained["date"].between("2022-05-27T02:00:00+02:00", "2022-05-29T05:30:00+02:00")]
+
     # Create empty plotly figure
     fig = go.Figure()
 
@@ -44,7 +52,20 @@ for s in series:
     fig.update_xaxes(title_text="Date")
     fig.update_yaxes(title_text="Count")
 
+    # y axis starts at 0 in plotly
+    fig.update_yaxes(autorange=False, range=[0, max(obtained["count"])*1.1])
+
+    # Set empty graph title
+    fig.update_layout(title_text="")
+
     # Plot obtained data in plotly using scatter
     fig.add_trace(go.Scatter(x=obtained["date"], y=obtained["count"], name="Aproximated"))
 
+    # Disable MathMenu ajax in pdf
+    import plotly.io as pio   
+    pio.kaleido.scope.mathjax = None
+
     fig.show()
+
+    # Save the figure in pdf file
+    fig.write_image("data1_results.pdf", width=2600/1.6, height=700/1.4)
